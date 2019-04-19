1b74358e1a688ae8fa8773498e6e86fd821d5bf6
Sadashi Ota <sadashi.ota.dev@gmail.com>
2019-04-20 00:29:58 +0900

    Refactor EffectActivity for kotlin.

---
+++ app/src/main/java/com/sadashi/apps/ui/samples/activities/EffectActivity.kt
@@ -0,0 +1,69 @@
+package com.sadashi.apps.ui.samples.activities
+
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.graphics.Point
+import android.os.Bundle
+import android.support.annotation.ColorRes
+import android.support.annotation.StringRes
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.view.ViewAnimationUtils
+import android.widget.Toast
+import com.sadashi.apps.ui.samples.R
+import kotlinx.android.synthetic.main.activity_effect.*
+
+class EffectActivity : AppCompatActivity() {
+    companion object {
+        private const val REVEAL_ANIMATION_DURATION = 500
+    }
+
+    private var currentReveal = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_effect)
+
+        nonEffect.setOnClickListener { showToast(R.string.message_tap_non_effect_text) }
+        rippleEffect.setOnClickListener { showToast(R.string.message_tap_ripple_effect_text) }
+        startReveal.setOnClickListener { revealAnimation() }
+    }
+
+    private fun revealAnimation() {
+        val cx = (startReveal.left + startReveal.right) / 2
+        val cy = (startReveal.top + startReveal.bottom) / 2
+
+        val display = windowManager.defaultDisplay
+        val point = Point()
+        display.getSize(point)
+        val radius = point.y
+
+        val nextReveal = !currentReveal
+        reveal.setBackgroundResource(getColorForReveal(nextReveal))
+
+        reveal.visibility = View.VISIBLE
+        val animator = ViewAnimationUtils.createCircularReveal(
+                reveal, cx, cy, 0f, radius.toFloat())
+        animator.addListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                reveal.visibility = View.INVISIBLE
+                base.setBackgroundResource(getColorForReveal(nextReveal))
+                currentReveal = nextReveal
+            }
+        })
+        animator.duration = REVEAL_ANIMATION_DURATION.toLong()
+        animator.start()
+    }
+
+    @ColorRes
+    private fun getColorForReveal(revealFlag: Boolean): Int {
+        return when (revealFlag) {
+            true -> R.color.colorReveal
+            false -> R.color.colorNormal
+        }
+    }
+
+    private fun showToast(@StringRes stringResId: Int) {
+        Toast.makeText(this, stringResId, Toast.LENGTH_SHORT).show()
+    }
+}
